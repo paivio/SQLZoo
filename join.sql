985f6997a55cc6b7f1c52d0b8032f87eb1f21183
Päivi Ojala <70027977+paivio@users.noreply.github.com>
2021-08-03 13:36:00 +0300

    Update join.sql

--- join.sql
+++ join.sql
@@ -69,6 +69,6 @@ team2,
 	SUM(CASE WHEN teamid=team2 THEN 1 
 	ELSE 0 
 	END) AS score2
-FROM game JOIN goal ON game.id=goal.matchid
+FROM game LEFT JOIN goal ON game.id=goal.matchid
 GROUP BY mdate, team1, team2
 ORDER BY mdate, team1, team2;
